e8658af03b4c85ae3d1e88276a282c9ad2a8259f
oktayuyar <oktay-uyar@hotmail.com>
2016-10-21 14:19:17 +0300

    activityler arası geçiş yapıldı

--- app/src/main/java/com/example/oktay/kaynakemniyetkontrolu/MainActivity.java
+++ app/src/main/java/com/example/oktay/kaynakemniyetkontrolu/MainActivity.java
@@ -5,14 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import com.example.oktay.kaynakemniyetkontrolu.SecondActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnHesapla,btnIncele;
+    private Button btnHesapla;
+    private EditText edtKuvvet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edtKuvvet=(EditText)findViewById(R.id.editText);
         onclick();
     }
 
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(View v) {
                         Intent intent = new Intent("com.example.oktay.kaynakemniyetkontrolu.SecondActivity");
+                        intent.putExtra("veri", edtKuvvet.getText().toString());
                         startActivity(intent);
                     }
                 }
--- app/src/main/java/com/example/oktay/kaynakemniyetkontrolu/SecondActivity.java
+++ app/src/main/java/com/example/oktay/kaynakemniyetkontrolu/SecondActivity.java
@@ -2,12 +2,26 @@ package com.example.oktay.kaynakemniyetkontrolu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
 
 public class SecondActivity extends AppCompatActivity {
-
+    private EditText edtkuvvet,edtcekme,edtegilme,edtkayma,edtemniyet;
+    Integer gelenKuvvet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        edtkuvvet=(EditText)findViewById(R.id.edtkuvvet);
+        edtcekme=(EditText)findViewById(R.id.edtcekme);
+        edtegilme=(EditText)findViewById(R.id.edtegilme);
+        edtkayma=(EditText)findViewById(R.id.edtkayma);
+        edtemniyet=(EditText)findViewById(R.id.edtemniyet);
+
+        edtkuvvet.setText(getIntent().getExtras().getString("veri")+"  Newton");
+        gelenKuvvet = Integer.parseInt((getIntent().getExtras().getString("veri")));
+
+
     }
 }
